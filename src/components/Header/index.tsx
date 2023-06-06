d31dc3d18e29ab5e12a406845e04af3e0b760492
Diego Barros <diegovlbarros@gmail.com>
2023-06-06 13:18:18 -0300

    Completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
 import "./styles.css";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
-      <div className="container">
+      <div className="header-content-container container">
         <Link to={"/"}>
           <h1 className="site-name">Meu Site</h1>
         </Link>
+        <nav className="navBar">
+          <NavLink to="/home" className={({isActive}) => isActive ? "menu-item menu-active" : "menu-item" }>
+            Inicio
+          </NavLink>
+          <NavLink to="/promotion" className={({isActive}) => isActive ? "menu-item menu-active" : "menu-item" }>
+            Promoção
+            </NavLink>
+          <NavLink to="/subscription" className={({isActive}) => isActive ? "menu-item menu-active" : "menu-item" }>
+            Participar
+            </NavLink>
+        </nav>
       </div>
     </header>
   );
 }
+
